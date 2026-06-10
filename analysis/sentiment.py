db435381b1bfadef05883b112707f2d43b4ff630
Brandon <brtroungvo@gmail.com>
2026-06-09 19:05:00 -0700

    Switched from list to dictionary in order to store the date that comments are posted on. Dates will be used for weighted average later on.

--- analysis/sentiment.py
+++ analysis/sentiment.py
@@ -3,10 +3,11 @@ import sys
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
-from dataCollection.read_json import comments
+from data.read_json import comments
 
 analyzer = SentimentIntensityAnalyzer()
+
+# Print out each comment and their polarity scores
 for sentence in comments:
     vs = analyzer.polarity_scores(sentence)
     print(f"{sentence} \n|---> {str(vs)} \n{'-'*50}")
-    
--- data/read_json.py
+++ data/read_json.py
@@ -7,8 +7,8 @@ filepath = os.path.join(DATA_COLLECTION, "london2026Comments.json")
 with open(filepath,'r',encoding = 'utf-8') as f:
     data = json.load(f)
 
-# Creates a list of strings to contain all the comments
-comments = []
+# Creates a dictionary with the comment as the key and the posting date as the value
+comments = {}
 for i in range(len(data)):
-    comments.append(data[i]["body"])
-    
+    if data[i]["body"] not in comments:
+        comments[data[i]["body"]] = [data[i]["created_utc"]]
